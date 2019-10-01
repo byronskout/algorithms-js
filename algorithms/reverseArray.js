d1b228a9b11301ba8332e4f6a44daad69f872258
byronskout <vyskoutaris@hotmail.com>
2019-10-01 15:54:21 +0100

    reverseArray.js

---
+++ algorithms/reverseArray.js
@@ -0,0 +1,8 @@
+function reverseArray(array) {
+    for (let i = 0; i < array.length / 2; i++) {
+        const temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+    return array;
+};
