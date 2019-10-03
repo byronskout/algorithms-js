ef1b74268cc1e7463a1ff547242aeb19c62526a0
byronskout <vyskoutaris@hotmail.com>
2019-10-03 15:24:27 +0100

    Reverse Words

---
+++ algorithms/reverseWords.js
@@ -0,0 +1,11 @@
+function reverseWords(string) {
+    const wordsReversed = [];
+
+    string.split(" ").forEach(word => {
+        let wordReversed = "";
+        for (let i = word.length - 1; i >= 0; i--) wordReversed += word[i];
+        wordsReversed.push(wordReversed);
+    });
+
+    return wordsReversed.join(" ");
+};
