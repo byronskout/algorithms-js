6a0d3c2e657b36ad2c97df2eae36919c69772385
byronskout <vyskoutaris@hotmail.com>
2019-10-09 20:48:32 +0100

    insertionSort.js

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,8 @@
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[i] < array[j]) array.splice(j, 0, array.splice(i, 1)[0]);
+    }
+  }
+  return array;
+}
