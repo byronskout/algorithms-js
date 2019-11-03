d6c8d191559509c9e6c48bff1d3ef158adb3e810
byronskout <vyskoutaris@hotmail.com>
2019-11-03 11:19:19 +0000

    countSetBits.js

---
+++ algorithms/countSetBits.js
@@ -0,0 +1,9 @@
+function countSetBits(originalNumber) {
+    let setBitsCount = 0;
+    let number = originalNumber;
+    while (number) {
+      setBitsCount += number & 1;
+      number >>= 1;
+    }
+    return setBitsCount;
+  }
