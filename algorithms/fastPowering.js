c53af917bf64c95e06889733aa6d246bffbed9d2
byronskout <vyskoutaris@hotmail.com>
2019-11-03 11:13:32 +0000

    fastPowering.js

---
+++ algorithms/fastPowering.js
@@ -0,0 +1,11 @@
+function fastPowering(base, power) {
+    if (power === 0) {
+      return 1;
+    }
+    if (power % 2 === 0) {
+      const multiplier = fastPowering(base, power / 2);
+      return multiplier * multiplier;
+    }
+    const multiplier = fastPowering(base, Math.floor(power / 2));
+    return multiplier * multiplier * base;
+  }
