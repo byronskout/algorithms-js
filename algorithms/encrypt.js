efc39b039c3dafb9563616ea44c394bfafadada0
byronskout <vyskoutaris@hotmail.com>
2019-11-07 16:58:40 +0000

    encrypt.js - encrypt added

--- algorithms/encrypt.js
+++ algorithms/encrypt.js
@@ -10,3 +10,24 @@ function isUpperCase(character) {
     return false;
   }
 }
+
+function encrypt(message, key) {
+  let result = "";
+  for (let i = 0, j = 0; i < message.length; i++) {
+    let c = message.charAt(i);
+    if (isLetter(c)){
+      if(isUpperCase(c)) {
+        result += String.fromCharCode((c.charCodeAt(0) + key.toUpperCase().charCodeAt(j) - 2 * 65) % 26 + 65);
+      } else {
+        result += String.fromCharCode((c.charCodeAt(0) + key.toLowerCase().charCodeAt(j) - 2 * 97) % 26 + 97);
+      }
+    } else {
+      result+=c;
+    }
+    j = ++j % key.length;
+  }
+  return result;
+}
+
+let messageEncrypt = encrypt('Hello World!', 'code');
+console.log(messageEncrypt);
