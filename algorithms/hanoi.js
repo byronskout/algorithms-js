aae123b33a10ebf91122b5125c08b69d10e0cf38
byronskout <vyskoutaris@hotmail.com>
2019-11-12 16:32:48 +0000

    hanoi.js

---
+++ algorithms/hanoi.js
@@ -0,0 +1,11 @@
+function hanoi(count, source, intermediate, goal, result) {
+    result = result || [];
+    if (count === 1) {
+      result.push([source, goal]);
+    } else {
+      hanoi(count - 1, source, goal, intermediate, result);
+      result.push([source, goal]);
+      hanoi(count - 1, intermediate, source, goal, result);
+    }
+    return result;
+  }
