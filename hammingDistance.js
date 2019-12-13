e0a48e40f50851cb5599383ba6897d50c23811bc
byronskout <vyskoutaris@hotmail.com>
2019-12-13 12:13:55 +0000

    hammingDistance mi

--- hammingDistance.js
+++ hammingDistance.js
@@ -1,3 +1,8 @@
 function sum(arr) {
     return arr.reduce(function(acc, e) { return acc + e; })
 }
+
+function mi(p, q) {
+    if (p.length != q.length) throw "Sequences need to be of same length!";
+    return p.map(function(e, i) { return [e, q[i]]; })
+}
